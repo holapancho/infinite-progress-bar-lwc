e353e4310269719eb4b2870d8c0f9c01e7cd32f4
Francisco Rojas <francisco.rojas.morales@gmail.com>
2021-04-11 22:10:48 +0000

    add sizes

--- src/infiniteProgressBar.js
+++ src/infiniteProgressBar.js
@@ -1,5 +1,34 @@
-import { LightningElement } from "lwc";
+import { LightningElement,api } from "lwc";
 
 export default class InfiniteProgressBar extends LightningElement {
-  
+
+  @api variant;
+  renderedCallback(){
+    if(!this.variant){
+      this.progressBar().classList.add('small');
+    }
+    else{
+      switch(this.variant){
+        case "x-small":
+          this.progressBar().classList.add('x-small');
+          break;
+        case "small":
+          this.progressBar().classList.add('small');
+          break;
+        case "medium":
+          this.progressBar().classList.add('medium');
+          break;
+        case "large":
+          this.progressBar().classList.add('large');
+          break;
+        default:
+          this.progressBar().classList.add('x-small');
+          break;
+      }
+    }
+  }
+
+  progressBar(){
+    return this.template.querySelector(".progress-bar");
+  }
 }
